376bbc52846e7cdf40d361b96ce72ab991626890
irem1905 <iremsila2424@gmail.com>
2024-06-14 17:51:09 +0300

    "Added basic HTML/CSS/JS structure"

---
+++ index.js
@@ -0,0 +1,13 @@
+const pen ="pembe" 
+if (pen !== "mavi"){
+    console.log("kalem mavidir");
+}
+else if (pen== "sarı"){
+    console.log("kalem sarı");
+}
+else{
+    console.log("kalem kırmızıdır");
+    const a= 15
+    console.log(a);
+}
+
